59dfd7748e9d1ce31a213b12f8843c4af2046591
Max Holzbauer <maxi.holzbauer@hotmail.com>
2021-03-18 18:14:50 +0100

    -f

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ function outputData(person) {
     row.classList.add('actor');
   }
   // Add a class to Austrians
-  if ((new RegExp(/American/gi)).test(person.profession)) {
-    row.classList.add('American');
+  if ((new RegExp(/american/gi)).test(person.profession)) {
+    row.classList.add('american');
   }
   // Add a class to Auto*
   if ((new RegExp(/auto/gi)).test(person.profession)) {
